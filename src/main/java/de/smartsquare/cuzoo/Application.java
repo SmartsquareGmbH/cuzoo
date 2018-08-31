e6568d7837c9a90de12bd7d7e12f3fca8fe78ec0
Dennis Dierkes <dierkes@smartsquare.de>
2018-08-31 11:20:02 +0200

    Add project scaffolding
    Spring Boot 2, Thymeleaf, Gradle, JPA, Security, Actuator and Web

---
+++ src/main/java/de/smartsquare/cuzoo/Application.java
@@ -0,0 +1,13 @@
+package de.smartsquare.cuzoo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        run(Application.class, args);
+    }
+}
---
+++ src/main/java/de/smartsquare/cuzoo/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all glue code
+ * e.g. application or configuration classes.
+ */
+package de.smartsquare.cuzoo;
