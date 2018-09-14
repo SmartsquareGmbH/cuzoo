176a28489150b1611f2c9813aee01bf8342fd81e
darivs <dtack192@gmail.com>
2018-09-14 13:35:45 +0200

    Add contact class, Import JSefa library for csv import

---
+++ src/main/java/de/smartsquare/cuzoo/csv/contact/Contact.java
@@ -0,0 +1,73 @@
+package de.smartsquare.cuzoo.csv.contact;
+
+import net.sf.jsefa.csv.annotation.CsvDataType;
+import net.sf.jsefa.csv.annotation.CsvField;
+
+@CsvDataType()
+public class Contact {
+    @CsvField(pos = 1)
+    private String name;
+
+    @CsvField(pos = 2)
+    private String company;
+
+    @CsvField(pos = 3)
+    private String role;
+
+    @CsvField(pos = 4)
+    private String address;
+
+    @CsvField(pos = 5)
+    private String mail;
+
+    @CsvField(pos = 6)
+    private String tel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
