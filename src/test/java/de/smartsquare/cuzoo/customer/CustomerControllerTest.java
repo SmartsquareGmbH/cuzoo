17cef4f1c6a2a865589e2453cb9fde6789a4cdac
darivs <dtack192@gmail.com>
2018-10-08 11:59:12 +0200

    Add CustomerControllerTest to test that imported list getting inserted correctly

---
+++ src/test/java/de/smartsquare/cuzoo/customer/CustomerControllerTest.java
@@ -0,0 +1,36 @@
+package de.smartsquare.cuzoo.customer;
+
+import de.smartsquare.cuzoo.csv.CSVImporter;
+import de.smartsquare.cuzoo.csv.CSVImporterTest;
+import de.smartsquare.cuzoo.csv.Company;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerControllerTest {
+
+    @Mock
+    private CustomerRepository mockCustomerRepository;
+    @InjectMocks
+    private CustomerController customerController;
+
+    @Test
+    public void that_imported_companies_getting_inserted_correctly() {
+        CSVImporter csvImporter = new CSVImporter();
+        List<Company> companies = csvImporter.importFrom(CSVImporterTest.class.getResourceAsStream("/TestCompanies.csv"), Company.class);
+
+        customerController.insertImportedCompanies(companies);
+
+        verify(mockCustomerRepository, times(3)).save(any(Customer.class));
+    }
+
+}
