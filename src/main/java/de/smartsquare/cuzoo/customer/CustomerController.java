43295a846a3c4f791eea3b3552ed4f917ef33552
darivs <dtack192@gmail.com>
2018-10-09 09:19:25 +0200

    Implement post on rest to upload .csv company file and convert it to customer class

--- src/main/java/de/smartsquare/cuzoo/customer/CustomerController.java
+++ src/main/java/de/smartsquare/cuzoo/customer/CustomerController.java
@@ -1,9 +1,14 @@
 package de.smartsquare.cuzoo.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.smartsquare.cuzoo.csv.CSVImporter;
+import de.smartsquare.cuzoo.csv.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -12,8 +17,37 @@ public class CustomerController {
 
     private final CustomerRepository repository;
 
+    @Autowired
     public CustomerController(final CustomerRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping("/import")
+    public final String postCompanyCSV(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            return "NOT OK";
+        } else {
+            CSVImporter csvImporter = new CSVImporter();
+            InputStream inputFile = new BufferedInputStream(file.getInputStream());
+
+            insertImportedCompanies(csvImporter.importFrom(inputFile, Company.class));
+            return "OK";
+        }
+    }
+
+    void insertImportedCompanies(List<Company> importedEntity) {
+        for (Company company : importedEntity) {
+            Customer customer = new Customer(
+                    company.getCompany(),
+                    company.getStreet(),
+                    company.getZipCode(),
+                    company.getPlace(),
+                    company.getHomepage(),
+                    company.getPurpose(),
+                    company.getOther());
+
+            repository.save(customer);
+        }
     }
 
     @GetMapping("/list")
