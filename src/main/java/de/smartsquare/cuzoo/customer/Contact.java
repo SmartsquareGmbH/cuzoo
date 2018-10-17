81d55c92ebb0f20a09dd1338b81cd9b3a575f96f
darivs <dtack192@gmail.com>
2018-10-17 14:47:02 +0200

    Add contact class to customer package

---
+++ src/main/java/de/smartsquare/cuzoo/customer/Contact.java
@@ -0,0 +1,126 @@
+package de.smartsquare.cuzoo.customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String company;
+    private String role;
+    private String address;
+    private String mail;
+    private String telephone;
+    private String lastContact;
+    private String lastAnswer;
+    private String comment;
+
+    public Contact() {
+    }
+
+    Contact(@NotNull @NotBlank final String name,
+            @NotNull @NotBlank final String company,
+            String role, String address, String mail, String telephone,
+            String lastContact, String lastAnswer, String comment) {
+        this.name = name;
+        this.company = company;
+        this.role = role;
+        this.address = address;
+        this.mail = mail;
+        this.telephone = telephone;
+        this.lastContact = lastContact;
+        this.lastAnswer = lastAnswer;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getLastContact() {
+        return lastContact;
+    }
+
+    public void setLastContact(String lastContact) {
+        this.lastContact = lastContact;
+    }
+
+    public String getLastAnswer() {
+        return lastAnswer;
+    }
+
+    public void setLastAnswer(String lastAnswer) {
+        this.lastAnswer = lastAnswer;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
