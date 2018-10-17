eed97095e271d855f06814657a6b4f9c65a732fd
darivs <dtack192@gmail.com>
2018-10-17 14:54:01 +0200

    Add ContactRepository and its test to customer package

---
+++ src/test/java/de/smartsquare/cuzoo/customer/ContactRepositoryTest.java
@@ -0,0 +1,27 @@
+package de.smartsquare.cuzoo.customer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ContactRepositoryTest {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Test
+    public final void  that_saves_contact_correctly() {
+        Contact darius = new Contact("Darius Tack", "Tack GmbH", "", "", "", "", "", "", "");
+
+        contactRepository.save(darius);
+
+        assertThat(contactRepository.findAll().size()).isEqualTo(1);
+    }
+
+}
