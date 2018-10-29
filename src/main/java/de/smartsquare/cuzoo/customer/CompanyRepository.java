ea451dec2964c218dde24fea0721e6afa9fc1cfd
darivs <dtack192@gmail.com>
2018-10-29 14:26:59 +0100

    Add query to find companies by name

--- src/main/java/de/smartsquare/cuzoo/customer/CompanyRepository.java
+++ src/main/java/de/smartsquare/cuzoo/customer/CompanyRepository.java
@@ -1,8 +1,13 @@
 package de.smartsquare.cuzoo.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Company c WHERE c.name = :companyName")
+    boolean existsByName(@Param("companyName") String companyName);
 }
