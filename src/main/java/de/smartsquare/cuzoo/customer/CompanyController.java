20966f0d11fada3490af8dea1c7e66f6192cc210
darivs <dtack192@gmail.com>
2018-10-30 14:46:25 +0100

    Update imports

--- src/main/java/de/smartsquare/cuzoo/customer/CompanyController.java
+++ src/main/java/de/smartsquare/cuzoo/customer/CompanyController.java
@@ -1,6 +1,7 @@
 package de.smartsquare.cuzoo.customer;
 
-import de.smartsquare.cuzoo.csv.*;
+import de.smartsquare.cuzoo.csv.CSVCompany;
+import de.smartsquare.cuzoo.csv.CSVImporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
