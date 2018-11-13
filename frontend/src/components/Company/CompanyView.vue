fa109c98dbeffe65010a8d31fd3846456902f0ef
darivs <dtack192@gmail.com>
2018-11-13 16:53:19 +0100

    Update detail page of companies

--- frontend/src/components/Company/CompanyView.vue
+++ frontend/src/components/Company/CompanyView.vue
@@ -8,15 +8,20 @@
                 </v-btn>
             </v-card>
         </v-flex>
-        <v-flex xs10>
-            <h1 class="display-1 text-xs-center">Unternehmensansicht</h1>
+        <v-flex xs4>
+            <h1 class="display-1 text-xs-center">Unternehmen</h1>
+        </v-flex>
+        <v-flex xs2>
+        </v-flex>
+        <v-flex xs4>
+            <h1 class="display-1 text-xs-center">Ansprechpartner</h1>
         </v-flex>
         <v-flex xs1>
         </v-flex>
         <v-flex xs1>
             <v-card dark color="green">
                 <v-card-text class="headline text-xs-center">
-                    <v-icon>business_center</v-icon>
+                    <v-icon size="30px">business_center</v-icon>
                 </v-card-text>  
             </v-card>
         </v-flex>
@@ -28,16 +33,14 @@
             </v-card>
         </v-flex>
         <v-flex xs6>
-            <v-card dark color="transparent" class="elevation-0">
-                <v-card-text class="headline text-xs-left">
-                    Unternehmenszweck
-                </v-card-text>
+            <v-card v-bind:key="contact.id" v-for="contact in contactsOfCompany" color="secondary" class="single-contacts mb-2">
+
             </v-card>
         </v-flex>
         <v-flex xs1>
             <v-card dark color="green" height="100%">
                 <v-card-text class="headline text-xs-center">
-                    <v-icon>home</v-icon>
+                    <v-icon size="30px">home</v-icon>
                 </v-card-text>  
             </v-card>
         </v-flex>
@@ -57,24 +60,15 @@
             </v-card>
         </v-flex>
         <v-flex xs6>
-            <v-card dark height="100%">
-                <v-card-text 
-                v-if="this.companies[this.companyId].purpose != null &&
-                this.companies[this.companyId].purpose != ''" 
-                class="title text-xs-left">
-                    {{ this.companies[this.companyId].purpose }}
-                </v-card-text>
-                <v-card-text
-                v-else
-                class="headline text-xs-left font-weight-light font-italic">
-                    N/A
-                </v-card-text>
+            <v-card dark color="transparent" class="elevation-0">
+
             </v-card>
         </v-flex>
+
         <v-flex xs1>
             <v-card dark color="green" height="212%" class="centered">
                 <v-card-text class="headline text-xs-center">
-                    <v-icon >place</v-icon>
+                    <v-icon size="30px">place</v-icon>
                 </v-card-text>  
             </v-card>
         </v-flex>
@@ -110,9 +104,7 @@
         </v-flex>
         <v-flex xs6>
             <v-card dark color="transparent" class="elevation-0">
-                <v-card-text class="headline text-xs-left">
-                    Sonstiges
-                </v-card-text>
+
             </v-card>
         </v-flex>
         <v-flex xs1>
@@ -135,12 +127,65 @@
                 </v-card-text>
             </v-card>
         </v-flex>
-        <v-flex xs6 align-end>
+        <v-flex xs6>
+            <v-card dark color="transparent">
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+                <v-card-text class="headline text-xs-left">
+                    <v-tooltip top>
+                    <v-icon color="info" size="36px" slot="activator">info</v-icon>
+                        Hauptfunktionen des Unternehmens
+                    </v-tooltip>
+                    Unternehmenszweck
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark height="100%">
+                <v-card-text 
+                v-if="this.companies[this.companyId].purpose != null &&
+                this.companies[this.companyId].purpose != ''" 
+                class="headline text-xs-left">
+                    {{ this.companies[this.companyId].purpose }}
+                </v-card-text>
+                <v-card-text
+                v-else
+                class="headline text-xs-left font-weight-light font-italic">
+                    N/A
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+                <v-card-text class="headline text-xs-left">
+                    <v-tooltip top>
+                    <v-icon color="info" size="36px" slot="activator">info</v-icon>
+                        Sonstige Angaben zum Unternehmen
+                    </v-tooltip>
+                    Sonstiges
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+            </v-card>
+        </v-flex>
+        <v-flex xs6>
             <v-card dark height="100%">
                 <v-card-text 
                 v-if="this.companies[this.companyId].other != null &&
                 this.companies[this.companyId].other != ''" 
-                class="title text-xs-left">
+                class="headline text-xs-left">
                     {{ this.companies[this.companyId].other }}
                 </v-card-text>
                 <v-card-text
@@ -150,11 +195,17 @@
                 </v-card-text>
             </v-card>
         </v-flex>
+        <v-flex xs6>
+            <v-card dark color="transparent" class="elevation-0">
+            </v-card>
+        </v-flex>
     </v-layout>
 </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
     computed: {
         companyId() {
@@ -165,6 +216,27 @@ export default {
         },
         homepage() {
             return "http://" + this.companies[this.companyId].homepage;
+        },
+        ...mapState(['contacts']),
+        contacts: {
+            get () {
+                return this.$store.state.contacts
+            },
+            set (contacts) {
+                this.$store.commit('storeContacts', contacts)
+            }
+        },
+        contactsOfCompany() {
+            return this.contacts.filter((contact) => {
+                console.log(contact);
+                if (contact.company.toLowerCase().includes((this.companies[this.$route.params.id].toLowerCase()))) {
+                    return contact.company.toLowerCase().includes((this.companies[this.$route.params.id].toLowerCase()));
+                } else {
+                    if (contact.company != null) {
+                        return contact.company.toLowerCase().includes((this.companies[this.$route.params.id].toLowerCase()));
+                    }
+                }
+            })
         }
     },
     methods : {
