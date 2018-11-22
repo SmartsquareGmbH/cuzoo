c6bfca829c0bfb14175b93893c5962f1687b4ca8
darivs <dtack192@gmail.com>
2018-11-22 14:46:05 +0100

    Update object parameters in tests

--- backend/src/test/java/de/smartsquare/cuzoo/customer/CompanyRepositoryTest.java
+++ backend/src/test/java/de/smartsquare/cuzoo/customer/CompanyRepositoryTest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -25,7 +23,7 @@ public class CompanyRepositoryTest {
 
     @Test
     public final void that_saves_company_correctly() {
-        Company darius = new Company("Tack GmbH", Collections.emptyList(), "", "", "", "", "", "", "");
+        Company darius = new Company("Tack GmbH", "",  "", "", "", "", "", "");
 
         companyRepository.save(darius);
 
