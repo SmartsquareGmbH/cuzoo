34df13ca6dabc81e54741cd25ee5e60c1d176690
darivs <dtack192@gmail.com>
2018-11-27 16:06:13 +0100

    Simplify existsByName query

--- backend/src/main/java/de/smartsquare/cuzoo/customer/CompanyRepository.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/CompanyRepository.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 interface CompanyRepository extends JpaRepository<Company, Long> {
-
-    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Company c WHERE c.name = :companyName")
-    boolean existsByName(@Param("companyName") String companyName);
+    boolean existsByName(String name);
 
     @Query("SELECT c FROM Company c WHERE c.name = :companyName ")
     Company findByName(@Param("companyName") String companyName);
