f65d6dcfdb74b21fbf1534e4b3acf5fecca34cb0
darivs <dtack192@gmail.com>
2018-12-03 16:30:01 +0100

    Add available information on result company card

--- frontend/src/components/Company/ResultCompany.vue
+++ frontend/src/components/Company/ResultCompany.vue
@@ -2,15 +2,49 @@
     <v-card 
     class="secondary mt-3 clickable"
     @click.native="viewCompany(company)">
-        <v-card-text class="headline text-xs-left">
+        <v-card-text 
+        class="headline text-xs-left">
             {{ company.name }}
-            <v-spacer/>
+            <div
+            v-if="company.zipCode || company.place || company.street"
+            class="headline font-weight-light text-xs-left">
+                <v-divider 
+                class="mt-2 mb-3"/>
+                <v-icon 
+                color="primary"
+                class="mr-1" 
+                size="30px">
+                    place
+                </v-icon> 
+                {{ company.zipCode }} {{ company.place }}, {{ company.street }}
+            </div>
+        </v-card-text>
+        <v-card 
+        class="blue-grey darken-1"
+        v-if="company.purpose || company.other">
+            <v-card-text 
+            class="headline text-xs-left font-weight-light">
+                {{ company.purpose }}
+                <v-spacer/>
+                {{ company.other }}
+            </v-card-text>
+        </v-card>
+        <v-card-text 
+        class="headline text-xs-left"
+        v-if="this.$parent.getContactsOfCompany(company.name).length > 0">
+            <v-icon 
+            color="primary" 
+            class="mr-2" 
+            size="30px">
+                people
+            </v-icon> 
             <v-chip
-            :color="randomColor(contact.id)"
-            class="mt-3"
+            class="mb-2"
+            color="primary"
             v-bind:key="contact.id"
             v-for="contact in this.$parent.getContactsOfCompany(company.name)">
-                <span class="headline black--text">
+                <span 
+                class="headline secondary--text">
                     {{ contact.name }}
                 </span>
             </v-chip>
@@ -19,13 +53,12 @@
 </template>
 
 <script>
-import {mapState} from 'vuex';
-
 export default {
     props: ['company'],
     data() {
         return {
-            colorCache: {}
+            colorCache: {},
+            companies: this.$parent.companies
         }
     },
     methods: {
@@ -33,6 +66,10 @@ export default {
             const random = () => Math.floor(256 * Math.random()) + 112;
 
             return this.colorCache[id] || (this.colorCache[id] = `rgb(${random()}, ${random()}, ${random()})`);
+        },
+        viewCompany: function (item) {
+            const index = this.$parent.companies.findIndex(company => company.id === item.id);
+            this.$router.push('/companies/' + (index));
         }
     }
 }
