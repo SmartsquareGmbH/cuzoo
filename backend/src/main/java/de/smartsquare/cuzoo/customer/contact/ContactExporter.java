8a8c153c6b817236120d3b7fd68364214135c0aa
darivs <dtack192@gmail.com>
2018-12-06 10:56:18 +0100

    Implement information writing at contact export

--- backend/src/main/java/de/smartsquare/cuzoo/customer/contact/ContactExporter.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/contact/ContactExporter.java
@@ -1,27 +1,41 @@
 package de.smartsquare.cuzoo.customer.contact;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
+import com.sun.org.apache.xml.internal.serialize.LineSeparator;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class ContactExporter {
-    private List<String> linesToWrite;
-    private File file;
-    private FileWriter fileWriter;
+    private static final String LINE_SEPARATOR = LineSeparator.Web;
 
-    ContactExporter() {
-        linesToWrite = new ArrayList<>();
-    }
-
-    File exportContactToTxt(Contact contact) {
+    Path exportContactToTxt(Contact contact) {
         String filename = contact.getName()
                 .replace(' ', '_')
                 .toLowerCase() + ".txt";
 
-        file = new File(filename);
+        Path path = Paths.get("src/main/resources/" + filename);
+        Charset charset = Charset.forName("UTF-8");
+        String content = getContactContent(contact);
 
-        return file;
+        try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
+            writer.write(content);
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+
+        return path;
     }
 
+    private String getContactContent(Contact contact) {
+        return "Name:\t\t " + contact.getName() + LINE_SEPARATOR +
+                "Unternehmen: " + contact.getCompany().getName() + LINE_SEPARATOR +
+                "Rolle:\t\t " + contact.getRole() + LINE_SEPARATOR +
+                "E-Mail:\t\t " + contact.getMail() + LINE_SEPARATOR +
+                "Telefon:\t " + contact.getTelephone() + LINE_SEPARATOR +
+                "Kommentar:\t " + contact.getComment() + LINE_SEPARATOR;
+    }
 }
--- backend/src/test/java/de/smartsquare/cuzoo/customer/contact/ContactExporterTest.java
+++ backend/src/test/java/de/smartsquare/cuzoo/customer/contact/ContactExporterTest.java
@@ -1,22 +1,44 @@
 package de.smartsquare.cuzoo.customer.contact;
 
 import de.smartsquare.cuzoo.customer.company.Company;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ContactExporterTest {
+    private Path file;
+    private Contact darius;
+
+    @Before
+    public void initialize() {
+        ContactExporter contactExporter = new ContactExporter();
+        Company smartsquare = new Company("Smartsquare GmbH", "", "", "", "", "", "");
+        darius = new Contact("Darius Tack", smartsquare, "Azubi", "darius@tack.de", "012345678910", "", "", "Kommentar");
+
+        file = contactExporter.exportContactToTxt(darius);
+    }
 
     @Test
     public void that_file_of_contact_is_getting_created() {
-        ContactExporter contactExporter = new ContactExporter();
-        Company smartsquare = new Company("Smartsquare GmbH", "", "", "", "", "", "");
-        Contact darius = new Contact("Darius Tack", smartsquare, "Azubi",  "darius@tack.de", "012345678910", "", "", "Kommentar");
+        assertThat(file.getFileName().toString()).isEqualTo("darius_tack.txt");
+    }
 
-        File file = contactExporter.exportContactToTxt(darius);
+    @Test
+    public void that_information_is_getting_written_to_text_file() throws IOException {
+        List<String> contents = Files.readAllLines(file);
 
-        System.out.println(file.getName());
-        assert file.getName().equals("darius_tack.txt");
+        assertThat(contents.get(0).contains(darius.getName())).isTrue();
+        assertThat(contents.get(1).contains(darius.getCompany().getName())).isTrue();
+        assertThat(contents.get(2).contains(darius.getRole())).isTrue();
+        assertThat(contents.get(3).contains(darius.getMail())).isTrue();
+        assertThat(contents.get(4).contains(darius.getTelephone())).isTrue();
+        assertThat(contents.get(5).contains(darius.getComment())).isTrue();
     }
 
 }
