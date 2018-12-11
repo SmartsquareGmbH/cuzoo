f33d7f8c20cc2b5b84f33fc50fd1a1c74c73a62b
darivs <dtack192@gmail.com>
2018-12-11 16:39:04 +0100

    Remove semicolons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
 import Vuetify from 'vuetify'
 import '@babel/polyfill'
-import Vue from "vue";
+import Vue from "vue"
 import './plugins/vuetify'
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false;
-
+  
 Vue.use(Vuetify, {
     theme: {
         primary: '#4FC3F7'
