f36d45b6b527d2039ad7ae0b574c34a69ccd12eb
darivs <dtack192@gmail.com>
2018-12-11 13:25:33 +0100

    Add contact point repository test

---
+++ backend/src/test/java/de/smartsquare/cuzoo/customer/points/CPointRepositoryTest.java
@@ -0,0 +1,40 @@
+package de.smartsquare.cuzoo.customer.points;
+
+import de.smartsquare.cuzoo.customer.contact.Contact;
+import de.smartsquare.cuzoo.customer.contact.ContactRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CPointRepositoryTest {
+
+    @Autowired
+    private CPointRepository cPointRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        cPointRepository.deleteAll();
+        contactRepository.deleteAll();
+    }
+
+    @Test
+    public final void that_saves_contact_point_correctly() {
+        Contact contact = new Contact("Darius Tack", "", "", "", "", "", "");
+        CPoint cPoint = new CPoint("Beratung", contact, "", "");
+
+        contactRepository.save(contact);
+        cPointRepository.save(cPoint);
+
+        assertThat(cPointRepository.findAll().size()).isEqualTo(1);
+    }
+}
