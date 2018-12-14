8e33bfe577b9c598b84aac3f22741a865ca894e9
darivs <dtack192@gmail.com>
2018-12-14 16:14:41 +0100

    Rename CompanyView page to Company

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,11 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Search from "./views/Search.vue";
-import CompanyTable from "./views/company/CompanyTable.vue";
-import CompanyView from "./views/company/CompanyView.vue";
-import ContactTable from "./views/contact/ContactTable.vue";
-import CPointList from "./views/point/CPointList.vue";
+import Vue from "vue"
+import Router from "vue-router"
+import Search from "./views/Search.vue"
+import CompanyTable from "./views/company/CompanyTable.vue"
+import Company from "./views/company/Company.vue"
+import ContactTable from "./views/contact/ContactTable.vue"
+import CPointList from "./views/point/CPointList.vue"
+import CPoint from "./views/point/CPoint"
 
 Vue.use(Router);
 
@@ -28,7 +29,7 @@ export default new Router({
         {
             path: "/companies/:id",
             name: "companyview",
-            component: CompanyView
+            component: Company
         },
         {
             path: "/:id",
