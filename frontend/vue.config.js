e4ee963304c95fc0323f5761a2cfcfb7503aa7bd
darivs <dtack192@gmail.com>
2018-12-14 11:48:24 +0100

    Apply runtime compiler in vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,12 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 module.exports = {
+  runtimeCompiler: true,
   configureWebpack: {
     plugins: [
       new VuetifyLoaderPlugin()
     ]
+  },
+
+  pluginOptions: {
   }
 }
