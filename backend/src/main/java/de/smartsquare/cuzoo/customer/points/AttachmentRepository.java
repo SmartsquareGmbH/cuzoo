8ae0a957a71cae22a8fe301872765a53775d2293
darivs <dtack192@gmail.com>
2018-12-20 17:10:44 +0100

    Add repository for attachments

---
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/points/AttachmentRepository.java
@@ -0,0 +1,8 @@
+package de.smartsquare.cuzoo.customer.points;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+}
