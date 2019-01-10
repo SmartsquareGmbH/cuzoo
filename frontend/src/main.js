abd3bb7e1fe489881403bf193ea360637477d0f1
darivs <dtack192@gmail.com>
2019-01-10 15:50:10 +0100

    Import vuetify library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import Vuetify from 'vuetify'
+import Vue from "vue"
+import Vuetify from 'vuetify/lib'
 import VueClip from 'vue-clip'
 import '@babel/polyfill'
-import Vue from "vue"
-import './plugins/vuetify'
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/src/stylus/app.styl'
+
 
 Vue.config.productionTip = false;
   
