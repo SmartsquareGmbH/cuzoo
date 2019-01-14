74e5f5330006267693f1e204018b44f9016569e8
darivs <dtack192@gmail.com>
2019-01-14 16:31:06 +0100

    Update contact point cards to show number of attachments

--- frontend/src/components/point/ContactPointCard.vue
+++ frontend/src/components/point/ContactPointCard.vue
@@ -8,19 +8,19 @@
             </v-card>
         </v-flex>
         <v-flex xs11>
-            <v-card 
-            @click.native="viewContactPoint(contactPoint)"
-            style="border-radius: 15px"
-            height="100%">
+            <v-card
+                    @click.native="viewContactPoint(contactPoint)"
+                    style="border-radius: 15px"
+                    height="100%">
                 <v-card-title class="secondary headline font-weight-light">
                     {{ contactPoint.title }} •
                     <span class="ml-2 mt-1 primary--text mr-2">{{ contactPoint.date }}</span> •
                     <v-icon class="ml-1">attach_file</v-icon>
-                    <span class="ml-1 mt-1 primary--text">0</span>
+                    <span class="ml-1 mt-1 primary--text">{{ fileNames.length }}</span>
                     <v-spacer></v-spacer>
                     <v-chip
-                    class="mb-2"
-                    color="white">
+                            class="mb-2"
+                            color="white">
                         <v-avatar class="info white--text" size="35px">
                             <v-icon>person</v-icon>
                         </v-avatar>
@@ -34,36 +34,53 @@
                 </v-card-text>
             </v-card>
         </v-flex>
-    </v-layout>    
+    </v-layout>
 </template>
 
 <script>
-export default {
-    props: ['contactPoint'],
-    data() {
-        return {
-            contactPoints: this.$parent.contactPoints
-        }
-    },
-    methods: {
-        getPointTypeIconOf: function (type) {
-            switch (type) {
-                case 'Telefon': return 'phone'
-                case 'E-Mail': return 'mail'
-                case 'Social Media': return 'share'
-                case 'Persönlich': return 'people'
+    import api from '@/utils/http-common'
+
+    export default {
+        props: ['contactPoint'],
+        data() {
+            return {
+                contactPoints: this.$parent.contactPoints,
+                company: this.$parent.company,
+                fileNames: []
             }
         },
-        viewContactPoint: function (item) {
-            const index = this.contactPoints.findIndex(contactPoint => contactPoint.id === item.id);
-            this.$router.push(this.$route.fullPath + '/' + (index));
+        mounted() {
+            api.get(`point/get/fileNames/${this.company.name}/${this.contactPoint.id - 1}`).then(response => {
+                if (response.data.length > 0) {
+                    this.fileNames = response.data;
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        methods: {
+            getPointTypeIconOf: function (type) {
+                switch (type) {
+                    case 'Telefon':
+                        return 'phone'
+                    case 'E-Mail':
+                        return 'mail'
+                    case 'Social Media':
+                        return 'share'
+                    case 'Persönlich':
+                        return 'people'
+                }
+            },
+            viewContactPoint: function (item) {
+                const index = this.contactPoints.findIndex(contactPoint => contactPoint.id === item.id);
+                this.$router.push(this.$route.fullPath + '/' + (index));
+            }
         }
     }
-}
 </script>
 
 <style>
-.clickable {
-    cursor: pointer;
-}
+    .clickable {
+        cursor: pointer;
+    }
 </style>
