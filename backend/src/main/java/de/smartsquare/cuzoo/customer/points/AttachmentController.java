b02b30b69f115a728324a2c26e63047748f70d74
darivs <dtack192@gmail.com>
2019-01-15 16:46:45 +0100

    Outsource file apis of contact point controller to own attachment controller

---
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/points/AttachmentController.java
@@ -0,0 +1,139 @@
+package de.smartsquare.cuzoo.customer.points;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/file")
+public class AttachmentController {
+    private final ContactPointRepository contactPointRepository;
+    private final AttachmentRepository attachmentRepository;
+
+    @Autowired
+    public AttachmentController(final ContactPointRepository contactPointRepository,
+                                final AttachmentRepository attachmentRepository) {
+        this.contactPointRepository = contactPointRepository;
+        this.attachmentRepository = attachmentRepository;
+    }
+
+    @PostMapping("/upload/{contactPointId}")
+    public final ResponseEntity<?> uploadFile(@PathVariable Long contactPointId,
+                                              @RequestParam("file") MultipartFile file) throws IOException {
+        Optional<ContactPoint> fileDestinationPoint = contactPointRepository.findById(contactPointId);
+
+        if (!fileDestinationPoint.isPresent() || file.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Attachment attachment = new Attachment(file.getOriginalFilename(), file.getBytes());
+
+        fileDestinationPoint.ifPresent(contactPoint -> {
+            checkFiles(contactPoint);
+            attachment.setContactPoint(contactPoint);
+        });
+
+        try {
+            attachmentRepository.save(attachment);
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void checkFiles(ContactPoint fileDestinationPoint) {
+        if (fileDestinationPoint.getFiles() == null) {
+            fileDestinationPoint.setFiles(new ArrayList<>());
+        }
+    }
+
+    @GetMapping("/download/{contactPointId}/{fileName}")
+    public final ResponseEntity<byte[]> downloadFile(@PathVariable Long contactPointId,
+                                                     @PathVariable String fileName) {
+        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
+
+        if (!fileOriginPoint.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (fileOriginPoint
+                .get().getFiles()
+                .stream()
+                .noneMatch(file -> file.getFilename().equals(fileName))) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(getFile(fileOriginPoint.get().getFiles(), fileName).getContent());
+    }
+
+    private Attachment getFile(List<Attachment> files, String filename) {
+        return files
+                .stream()
+                .filter(file -> file.getFilename().equals(filename))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @DeleteMapping("/delete/{contactPointId}/{fileName}")
+    public final ResponseEntity<?> deleteFileOfContactPoint(@PathVariable Long contactPointId,
+                                                            @PathVariable String fileName) {
+        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
+
+        if (!fileOriginPoint.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (fileOriginPoint
+                .get().getFiles()
+                .stream()
+                .noneMatch(file -> file.getFilename().equals(fileName))) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        fileOriginPoint
+                .get().getFiles()
+                .stream()
+                .filter(file -> file.getFilename()
+                        .equals(fileName))
+                .findFirst()
+                .ifPresent(attachmentRepository::delete);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/get/names/{contactPointId}")
+    public final ResponseEntity<List<String>> getAllFileNamesOfContactPoint(@PathVariable Long contactPointId) {
+        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
+
+        if (!fileOriginPoint.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (fileOriginPoint.get().getFiles().size() < 1) {
+            return ResponseEntity.ok(new ArrayList<>());
+        } else {
+            return ResponseEntity.ok(getFileNames(fileOriginPoint.get().getFiles()));
+        }
+    }
+
+    private List<String> getFileNames(List<Attachment> files) {
+        if (files.size() > 0) {
+            List<String> fileNames = new ArrayList<>();
+
+            files.forEach(file -> fileNames.add(file.getFilename()));
+
+            return fileNames;
+        } else {
+            return null;
+        }
+    }
+}
--- backend/src/main/java/de/smartsquare/cuzoo/customer/points/ContactPointController.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/points/ContactPointController.java
@@ -9,29 +9,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/point")
 public class ContactPointController {
     private final ContactPointRepository contactPointRepository;
-    private final AttachmentRepository attachmentRepository;
     private final ContactRepository contactRepository;
     private final CompanyRepository companyRepository;
 
     @Autowired
-    public ContactPointController(final ContactPointRepository contactPointRepository, final AttachmentRepository attachmentRepository,
+    public ContactPointController(final ContactPointRepository contactPointRepository,
                                   final ContactRepository contactRepository, final CompanyRepository companyRepository) {
         this.contactPointRepository = contactPointRepository;
-        this.attachmentRepository = attachmentRepository;
         this.contactRepository = contactRepository;
         this.companyRepository = companyRepository;
     }
@@ -73,122 +66,6 @@ public class ContactPointController {
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    @PostMapping("/upload/{companyName}/{contactPointId}")
-    public final ResponseEntity<?> uploadFile(@PathVariable String companyName, @PathVariable Long contactPointId,
-                                              @RequestParam("file") MultipartFile file) throws IOException {
-        if (!companyRepository.existsByName(companyName) || file.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        Optional<ContactPoint> fileDestinationPoint = contactPointRepository.findById(contactPointId);
-
-        if (!fileDestinationPoint.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        Attachment attachment = new Attachment(file.getOriginalFilename(), file.getBytes());
-
-        fileDestinationPoint.ifPresent(contactPoint -> {
-            checkFiles(contactPoint);
-            attachment.setContactPoint(contactPoint);
-        });
-
-        try {
-            attachmentRepository.save(attachment);
-
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (DataAccessException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    private void checkFiles(ContactPoint fileDestinationPoint) {
-        if (fileDestinationPoint.getFiles() == null) {
-            fileDestinationPoint.setFiles(new ArrayList<>());
-        }
-    }
-
-    @GetMapping("/download/{contactPointId}/{fileName}")
-    public final ResponseEntity<byte[]> downloadFile(@PathVariable Long contactPointId,
-                                                     @PathVariable String fileName) {
-        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
-
-        if (!fileOriginPoint.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        if (fileOriginPoint
-                .get().getFiles()
-                .stream()
-                .noneMatch(file -> file.getFilename().equals(fileName))) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return ResponseEntity.ok(getFile(fileOriginPoint.get().getFiles(), fileName).getContent());
-    }
-
-    private Attachment getFile(List<Attachment> files, String filename) {
-        return files
-                .stream()
-                .filter(file -> file.getFilename().equals(filename))
-                .findFirst()
-                .orElse(null);
-    }
-
-    @GetMapping("/get/fileNames/{contactPointId}")
-    public final ResponseEntity<List<String>> getAllFileNamesOfContactPoint(@PathVariable Long contactPointId) {
-        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
-
-        if (!fileOriginPoint.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        if (fileOriginPoint.get().getFiles().size() < 1) {
-            return ResponseEntity.ok(new ArrayList<>());
-        } else {
-            return ResponseEntity.ok(getFileNames(fileOriginPoint.get().getFiles()));
-        }
-    }
-
-    private List<String> getFileNames(List<Attachment> files) {
-        if (files.size() > 0) {
-            List<String> fileNames = new ArrayList<>();
-
-            files.forEach(file -> fileNames.add(file.getFilename()));
-
-            return fileNames;
-        } else {
-            return null;
-        }
-    }
-
-    @DeleteMapping("/file/delete/{contactPointId}/{fileName}")
-    public final ResponseEntity<?> deleteFileOfContactPoint(@PathVariable Long contactPointId,
-                                                            @PathVariable String fileName) {
-        Optional<ContactPoint> fileOriginPoint = contactPointRepository.findById(contactPointId);
-
-        if (!fileOriginPoint.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        if (fileOriginPoint
-                .get().getFiles()
-                .stream()
-                .noneMatch(file -> file.getFilename().equals(fileName))) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        fileOriginPoint
-                .get().getFiles()
-                .stream()
-                .filter(file -> file.getFilename()
-                        .equals(fileName))
-                .findFirst()
-                .ifPresent(attachmentRepository::delete);
-
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/get")
