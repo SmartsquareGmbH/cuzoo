c80c06054a712632e599bde8b0a509e9e5014349
darivs <dtack192@gmail.com>
2019-01-24 17:18:16 +0100

    Add label repository

---
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/label/LabelRepository.java
@@ -0,0 +1,16 @@
+package de.smartsquare.cuzoo.customer.label;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LabelRepository extends JpaRepository<Label, Long> {
+
+    @Query("SELECT l FROM Label l WHERE l.title = :title AND l.contactPoint <> null")
+    Optional<Label> findForContactPointByTitle(@Param("title") String title);
+
+}
