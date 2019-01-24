9604d7dff32b7b7f0fafebf2ed996dc1de9d9ac1
darivs <dtack192@gmail.com>
2019-01-24 17:18:46 +0100

    Add query to find all labels of contact point entity

--- backend/src/main/java/de/smartsquare/cuzoo/customer/contactpoint/ContactPointRepository.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/contactpoint/ContactPointRepository.java
@@ -1,17 +1,16 @@
 package de.smartsquare.cuzoo.customer.contactpoint;
 
+import de.smartsquare.cuzoo.customer.label.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ContactPointRepository extends JpaRepository<ContactPoint, Long> {
 
-    @Query("SELECT c FROM ContactPoint c WHERE c.contact.company.name = :companyName ORDER BY c.date")
-    Optional<List<ContactPoint>> findContactPointsByCompanyName(@Param("companyName") String companyName);
+    @Query("SELECT c.labels FROM ContactPoint c")
+    List<Label> findAllLabels();
 
 }
