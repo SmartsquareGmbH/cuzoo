4dac9dabf9d9b7d90bb48f95aed099ec25be398d
darivs <dtack192@gmail.com>
2019-01-29 15:41:11 +0100

    Remove unused hql query to find all labels of contact points

--- backend/src/main/java/de/smartsquare/cuzoo/customer/contactpoint/ContactPointRepository.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/contactpoint/ContactPointRepository.java
@@ -1,16 +1,9 @@
 package de.smartsquare.cuzoo.customer.contactpoint;
 
-import de.smartsquare.cuzoo.customer.label.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ContactPointRepository extends JpaRepository<ContactPoint, Long> {
-
-    @Query("SELECT c.labels FROM ContactPoint c")
-    List<Label> findAllLabels();
 
 }
