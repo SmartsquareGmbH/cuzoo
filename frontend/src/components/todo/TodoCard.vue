e831ed98ef596084c0052cbb24e1ac0f5b20bc67
darivs <dtack192@gmail.com>
2019-02-01 11:32:43 +0100

    Add button animation on hovering over todo card

--- frontend/src/components/todo/TodoCard.vue
+++ frontend/src/components/todo/TodoCard.vue
@@ -1,26 +1,38 @@
 <template>
     <v-scroll-x-transition>
         <v-flex xs12 v-if="todo.done === false">
-            <v-card style="border-radius: 15px" height="100%" :color="getUrgency(todo.expiration)">
-                <v-btn absolute top right fab small color="secondary" @click="taskIsDone(todo)"
-                       class="elevation-12">
-                    <v-icon size="24px" color="success" class="ml-3">
-                        done_outline
-                    </v-icon>
-                </v-btn>
-                <v-card-text v-if="getUrgency(todo.expiration) !== 'secondary'" class="secondary--text headline text-xs-left">
-                    <v-icon size="30px" class="mr-2" color="secondary">
-                        {{ getUrgency(todo.expiration) }}
-                    </v-icon>
-                    {{ todo.description }}
-                </v-card-text>
-                <v-card-text v-else class="white--text headline text-xs-left">
-                    <v-icon size="30px" class="mr-2" color="white">
-                        info
-                    </v-icon>
-                    {{ todo.description }}
-                </v-card-text>
-            </v-card>
+            <v-hover>
+                <v-card slot-scope="{ hover }"
+                        style="border-radius: 15px"
+                        :color="getUrgency(todo.expiration)"
+                        height="100%">
+                    <v-scroll-x-transition>
+                        <v-btn v-if="hover"
+                               absolute top right fab small
+                               color="secondary"
+                               @click="taskIsDone(todo)"
+                               class="elevation-12">
+                            <v-icon size="24px" color="success" class="ml-3">
+                                done_outline
+                            </v-icon>
+                        </v-btn>
+                    </v-scroll-x-transition>
+                    <v-card-text
+                            v-if="getUrgency(todo.expiration) !== 'secondary'"
+                            class="secondary--text headline text-xs-left">
+                        <v-icon size="30px" class="mr-2" color="secondary">
+                            {{ getUrgency(todo.expiration) }}
+                        </v-icon>
+                        {{ todo.description }}
+                    </v-card-text>
+                    <v-card-text v-else class="white--text headline text-xs-left">
+                        <v-icon size="30px" class="mr-2" color="white">
+                            info
+                        </v-icon>
+                        {{ todo.description }}
+                    </v-card-text>
+                </v-card>
+            </v-hover>
         </v-flex>
     </v-scroll-x-transition>
 </template>
