46d8571273448680140edea50cafb42a4cb44496
darivs <dtack192@gmail.com>
2019-02-01 11:29:26 +0100

    Apply hover animation on type icon at contact point card

--- frontend/src/components/contactpoint/ContactPointCard.vue
+++ frontend/src/components/contactpoint/ContactPointCard.vue
@@ -1,60 +1,63 @@
 <template>
     <v-scroll-x-reverse-transition>
-    <v-layout row wrap class="clickable">
-        <v-flex xs1>
-            <v-card color="info" height="100%">
-                <v-card-text class="pt-4">
-                    <v-icon large class="pt-4">{{ getPointTypeIconOf(contactPoint.type) }}</v-icon>
-                </v-card-text>
-            </v-card>
-        </v-flex>
-        <v-flex xs11>
-            <v-hover>
-            <v-card
-                    slot-scope="{ hover }"
-                    :class="`elevation-${hover ? 6 : 2}`"
-                    @click="viewContactPoint(contactPoint)"
-                    height="100%">
-                <v-scroll-x-transition>
-                    <v-btn v-if="hover" absolute right top fab small color="secondary" @click.stop="editContactPoint()"
-                           class="elevation-12 mr-5">
-                        <v-icon size="24px" color="white">
-                            edit
-                        </v-icon>
-                    </v-btn>
-                </v-scroll-x-transition>
-                <v-scroll-x-transition>
-                    <v-btn v-if="hover" absolute right top fab small color="secondary" @click.stop="deleteContactPoint()"
-                           class="elevation-12">
-                        <v-icon size="24px" color="error">
-                            delete
-                        </v-icon>
-                    </v-btn>
-                </v-scroll-x-transition>
-                <v-card-title class="secondary headline font-weight-light">
-                    {{ contactPoint.title }} •
-                    <span class="ml-2 primary--text mr-2">{{ dateFormatted }}</span> •
-                    <v-icon class="ml-1">attach_file</v-icon>
-                    <span class="ml-1 primary--text">{{ fileNames.length }}</span>
-                    <v-spacer></v-spacer>
-                    <v-chip
-                            :class="`mt-${hover ? 4 : 0} mb-${hover ? 0 : 2}`"
-                            color="white">
-                        <v-avatar class="info white--text" size="35px">
-                            <v-icon>person</v-icon>
-                        </v-avatar>
-                        <span class="headline secondary--text">
-                            {{ contactPoint.contact.name }}
-                        </span>
-                    </v-chip>
-                </v-card-title>
-                <v-card-text class="subheading text-xs-left text-truncate" width="20%">
-                    {{ contactPoint.comment }}
-                </v-card-text>
-            </v-card>
-            </v-hover>
-        </v-flex>
-    </v-layout>
+        <v-hover>
+            <v-layout row wrap
+                      class="clickable"
+                      slot-scope="{ hover }">
+                <v-flex xs1>
+                    <v-card color="info" height="100%">
+                        <v-card-text class="pt-4">
+                            <v-icon large :class="`pt-4 mt-${hover ? 2 : 0}`">
+                                {{ getPointTypeIconOf(contactPoint.type) }}
+                            </v-icon>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+                <v-flex xs11>
+                    <v-card
+                            :class="`elevation-${hover ? 6 : 2}`"
+                            @click="viewContactPoint(contactPoint)"
+                            height="100%">
+                        <v-scroll-x-transition>
+                            <v-btn v-if="hover" absolute right top fab small color="secondary" @click.stop="editContactPoint()"
+                                   class="elevation-12 mr-5">
+                                <v-icon size="24px" color="white">
+                                    edit
+                                </v-icon>
+                            </v-btn>
+                        </v-scroll-x-transition>
+                        <v-scroll-x-transition>
+                            <v-btn v-if="hover" absolute right top fab small color="secondary" @click.stop="deleteContactPoint()"
+                                   class="elevation-12">
+                                <v-icon size="24px" color="error">
+                                    delete
+                                </v-icon>
+                            </v-btn>
+                        </v-scroll-x-transition>
+                        <v-card-title class="secondary headline font-weight-light">
+                            {{ contactPoint.title }} •
+                            <span class="ml-2 primary--text mr-2">{{ dateFormatted }}</span> •
+                            <v-icon class="ml-1">attach_file</v-icon>
+                            <span class="ml-1 primary--text">{{ fileNames.length }}</span>
+                            <v-spacer></v-spacer>
+                            <v-chip
+                                    :class="`mt-${hover ? 4 : 0} mb-${hover ? 0 : 2}`"
+                                    color="white">
+                                <v-avatar class="info white--text" size="35px">
+                                    <v-icon>person</v-icon>
+                                </v-avatar>
+                                <span class="headline secondary--text">
+                                    {{ contactPoint.contact.name }}
+                                </span>
+                            </v-chip>
+                        </v-card-title>
+                        <v-card-text class="subheading text-xs-left text-truncate" width="20%">
+                            {{ contactPoint.comment }}
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-hover>
     </v-scroll-x-reverse-transition>
 </template>
 
