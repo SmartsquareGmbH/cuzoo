08c9565d51a07b1745f53a0b8b6ca3df31abcf99
darivs <dtack192@gmail.com>
2019-02-04 11:24:51 +0100

    Add scroll x transition to company card component

--- frontend/src/components/company/CompanyCard.vue
+++ frontend/src/components/company/CompanyCard.vue
@@ -1,57 +1,59 @@
 <template>
-    <v-card
-    style="border-radius: 10px"
-    class="secondary mt-3 mb-4 clickable"
-    @click.native="viewContactPoints(company)"
-    hover>
-        <v-card-text 
-        class="headline text-xs-left">
-            {{ company.name }}
-            <v-divider
-            class="mt-2 mb-3"/>
-            <div
-            v-if="company.zipCode || company.place || company.street"
-            class="headline font-weight-light text-xs-left">
-                <v-icon 
+    <v-scroll-x-transition>
+        <v-card
+                style="border-radius: 10px"
+                class="secondary mt-3 mb-4 clickable"
+                @click.native="viewContactPoints(company)"
+                hover>
+            <v-card-text
+            class="headline text-xs-left">
+                {{ company.name }}
+                <v-divider
+                class="mt-2 mb-3"/>
+                <div
+                v-if="company.zipCode || company.place || company.street"
+                class="headline font-weight-light text-xs-left">
+                    <v-icon
+                    color="primary"
+                    class="mr-1"
+                    size="30px">
+                        place
+                    </v-icon>
+                    {{ company.zipCode }} {{ company.place }}, {{ company.street }}
+                </div>
+            </v-card-text>
+            <v-card
+            color="#377D93"
+            v-if="company.description || company.other">
+                <v-card-text
+                class="headline text-xs-left font-weight-light">
+                    {{ company.description }}
+                    <v-spacer/>
+                    {{ company.other }}
+                </v-card-text>
+            </v-card>
+            <v-card-text
+            class="headline text-xs-left"
+            v-if="this.$parent.getContactsOfCompany(company.name).length > 0">
+                <v-icon
                 color="primary"
-                class="mr-1" 
+                class="mr-2"
                 size="30px">
-                    place
-                </v-icon> 
-                {{ company.zipCode }} {{ company.place }}, {{ company.street }}
-            </div>
-        </v-card-text>
-        <v-card 
-        color="#377D93"
-        v-if="company.description || company.other">
-            <v-card-text 
-            class="headline text-xs-left font-weight-light">
-                {{ company.description }}
-                <v-spacer/>
-                {{ company.other }}
+                    people
+                </v-icon>
+                <v-chip
+                class="mb-2"
+                color="primary"
+                v-bind:key="contact.id"
+                v-for="contact in this.$parent.getContactsOfCompany(company.name)">
+                    <span
+                    class="headline secondary--text">
+                        {{ contact.name }}
+                    </span>
+                </v-chip>
             </v-card-text>
         </v-card>
-        <v-card-text 
-        class="headline text-xs-left"
-        v-if="this.$parent.getContactsOfCompany(company.name).length > 0">
-            <v-icon 
-            color="primary" 
-            class="mr-2" 
-            size="30px">
-                people
-            </v-icon> 
-            <v-chip
-            class="mb-2"
-            color="primary"
-            v-bind:key="contact.id"
-            v-for="contact in this.$parent.getContactsOfCompany(company.name)">
-                <span 
-                class="headline secondary--text">
-                    {{ contact.name }}
-                </span>
-            </v-chip>
-        </v-card-text>
-    </v-card>
+    </v-scroll-x-transition>
 </template>
 
 <script>
