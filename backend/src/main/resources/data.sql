7232350aa602e0d0197919c08edc36b5731dca70
darivs <dtack192@gmail.com>
2019-02-07 11:59:00 +0100

    Change ids at data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,39 +1,39 @@
 INSERT INTO company (id, name, street, zipcode, place, homepage, description, other) VALUES
-(1, 'Anders GmbH', 'In der Wo 122', '32421', 'Andersen', '', 'Andere Softwareentwicklung', 'Java, HTML5, CSS3, Anderes'),
-(2, 'Ben & Biggs AG', 'An der Wann 39', '13913', 'Biggsendorf', 'www.benbiggs.com', 'Softwareentwicklung', 'C#, C++, Java'),
-(3, 'Chlor Claud', 'Unter dem Wer 1', '11111', 'Chlorhausen', 'www.claud.com', 'Vertrieb', 'Chlor, Cloud, Kotlin');
+(11111, 'Anders GmbH', 'In der Wo 122', '32421', 'Andersen', '', 'Andere Softwareentwicklung', 'Java, HTML5, CSS3, Anderes'),
+(13337, 'Ben & Biggs AG', 'An der Wann 39', '13913', 'Biggsendorf', 'www.benbiggs.com', 'Softwareentwicklung', 'C#, C++, Java'),
+(66666, 'Chlor Claud', 'Unter dem Wer 1', '11111', 'Chlorhausen', 'www.claud.com', 'Vertrieb', 'Chlor, Cloud, Kotlin');
 
 INSERT INTO contact (id, name, role, mail, telephone, company_id) VALUES
-(1, 'Alfred Anders', 'Geschaeftsfuehrer', 'alfred@anders.de', '134782442', 1),
-(2, 'Ben Big', 'Softwarearchitekt', 'ben@benbiggs.com', '428743409', 2),
-(3, 'Claudia Chlor', 'Freiberufler', 'chlor@claud.de', '234984809', 3);
+(11111, 'Alfred Anders', 'Geschaeftsfuehrer', 'alfred@anders.de', '134782442', 11111),
+(13337, 'Ben Big', 'Softwarearchitekt', 'ben@benbiggs.com', '428743409', 13337),
+(66666, 'Claudia Chlor', 'Freiberufler', 'chlor@claud.de', '234984809', 66666);
 
 INSERT INTO contact_point (id, title, type, date, contact_id, comment) VALUES
-(1, 'Kennenlernen', 'Telefon', '2019-02-03 00:00:00', 1,
+(11111, 'Kennenlernen', 'Telefon', '2019-02-03 00:00:00', 11111,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(2, 'Besprechung', 'Social Media', '2019-02-05 00:00:00', 1,
+(13337, 'Besprechung', 'Social Media', '2019-02-05 00:00:00', 11111,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(3, 'Pöbeln', 'E-Mail', '2019-01-02 00:00:00', 3,
+(66666, 'Pöbeln', 'E-Mail', '2019-01-02 00:00:00', 66666,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(4, 'Frohe Weihnachten', 'Persönlich', '2019-01-02 00:00:00', 2,
+(99999, 'Frohe Weihnachten', 'Persönlich', '2019-01-02 00:00:00', 13337,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
 
 INSERT INTO label (id, title) VALUES
-(1, 'Weihnachten 2018'),
-(2, 'Cloud Lab Bielefeld'),
-(3, 'Cloud-Flyer'),
-(4, 'JUG');
+(11111, 'Weihnachten 2018'),
+(13337, 'Cloud Lab Bielefeld'),
+(66666, 'Cloud-Flyer'),
+(99999, 'JUG');
 
 INSERT INTO contact_point_labels (contact_point_id, label_id) VALUES
-(1, 2),
-(3, 3),
-(3, 1),
-(2, 4),
-(2, 3),
-(3, 4),
-(4, 1);
+(11111, 13337),
+(66666, 66666),
+(66666, 11111),
+(13337, 99999),
+(13337, 66666),
+(66666, 99999),
+(99999, 11111);
 
 INSERT INTO todo (id, description, company_id, expiration, reminder, done) VALUES
-(1, 'Alfred Bescheid geben', 1, '2019-02-05 00:00:0', '2019-02-04 00:00:0', false),
-(2, 'Mama anrufen', 1, '2020-02-05 00:00:0', '2020-02-04 00:00:0', false),
-(3, 'Organisieren', 1, '2019-02-07 00:00:0', '2010-02-04 00:00:0', false);
+(11111, 'Alfred Bescheid geben', 11111, '2019-02-05 00:00:0', '2019-02-04 00:00:0', false),
+(13337, 'Mama anrufen', 11111, '2020-02-05 00:00:0', '2020-02-04 00:00:0', false),
+(66666, 'Organisieren', 11111, '2019-02-07 00:00:0', '2010-02-04 00:00:0', false);
