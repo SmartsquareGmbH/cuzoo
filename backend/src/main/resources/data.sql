20636bc80a6072cf440738d4a0caab28999324d9
darivs <dtack192@gmail.com>
2019-02-11 13:25:29 +0100

    Add media type label relations to init database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,19 +8,20 @@ INSERT INTO contact (id, name, role, mail, telephone, company_id) VALUES
 (13337, 'Ben Big', 'Softwarearchitekt', 'ben@benbiggs.com', '428743409', 13337),
 (66666, 'Claudia Chlor', 'Freiberufler', 'chlor@claud.de', '234984809', 66666);
 
-INSERT INTO contact_point (id, title, type, date, contact_id, comment) VALUES
-(11111, 'Kennenlernen', 'Telefon', '2019-02-03 00:00:00', 11111,
+INSERT INTO contact_point (id, title, date, contact_id, comment) VALUES
+(11111, 'Kennenlernen', '2019-02-03 00:00:00', 11111,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(13337, 'Besprechung', 'Social Media', '2019-02-05 00:00:00', 11111,
+(13337, 'Besprechung', '2019-02-05 00:00:00', 11111,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(66666, 'Pöbeln', 'E-Mail', '2019-01-02 00:00:00', 66666,
+(66666, 'Pöbeln', '2019-01-02 00:00:00', 66666,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'),
-(99999, 'Frohe Weihnachten', 'Persönlich', '2019-01-02 00:00:00', 13337,
+(99999, 'Frohe Weihnachten', '2019-01-02 00:00:00', 13337,
  'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
 
 INSERT INTO label (id, title) VALUES
 (11111, 'Weihnachten 2018'),
 (13337, 'Cloud Lab Bielefeld'),
+(44444, 'Xing'),
 (66666, 'Cloud-Flyer'),
 (99999, 'JUG');
 
@@ -32,6 +33,12 @@ INSERT INTO contact_point_labels (contact_point_id, label_id) VALUES
 (13337, 66666),
 (66666, 99999),
 (99999, 11111);
+
+INSERT INTO contact_point_types (contact_point_id, label_id) VALUES
+(11111, 44444),
+(13337, 44444),
+(66666, 99999),
+(99999, 13337);
 
 INSERT INTO todo (id, description, company_id, expiration, reminder, done) VALUES
 (11111, 'Alfred Bescheid geben', 11111, '2019-02-05 00:00:0', '2019-02-04 00:00:0', false),
