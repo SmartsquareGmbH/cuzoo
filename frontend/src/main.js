1d8d8b74b4e2e2ada55078009d0d45ce2b565381
darivs <dtack192@gmail.com>
2019-02-21 17:46:20 +0100

    Implement vue perfect scrollbar dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import Vue from "vue"
-import Vuetify from 'vuetify/lib'
-import VueClip from 'vue-clip'
-import App from "./App.vue"
-import router from "./router"
-import store from './plugins/vuex'
-import './plugins/datefns'
-import '@babel/polyfill'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from 'vuetify/lib';
+import VueClip from 'vue-clip';
+import App from "./App.vue";
+import router from "./router";
+import store from './plugins/vuex';
+import './plugins/datefns';
+import '@babel/polyfill';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'vuetify/src/stylus/app.styl';
 
 
 Vue.config.productionTip = false;
