8a9f810a8917ddd24dfe478f1146c92d661304af
darivs <dtack192@gmail.com>
2019-03-08 13:41:56 +0100

    Add small chip component

---
+++ frontend/src/components/main/small/Chip.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-chip :class="`subheading ${fontColor}--text my-1`"
+            small>
+        <slot/>
+    </v-chip>
+</template>
+
+<script>
+    export default {
+        props: ['fontColor']
+    }
+</script>
