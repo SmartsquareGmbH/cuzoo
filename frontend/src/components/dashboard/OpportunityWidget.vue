5e03906e4ea799b701689c0bcd740327f0c62bc5
darivs <dtack192@gmail.com>
2019-03-21 17:26:05 +0100

    Add opportunity dashboard widget and card

---
+++ frontend/src/components/dashboard/OpportunityWidget.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-layout row wrap class="text-xs-right">
+        <v-flex xs12 class="text-xs-left more-padding-top">
+            <v-icon color="primary" size="24px">bubble_chart</v-icon>
+            <span class="headline font-weight-light">
+                Opportunities
+            </span>
+        </v-flex>
+    </v-layout>
+</template>
