003879dfb353919a0039cf8331bfa6bed5c6186d
darivs <dtack192@gmail.com>
2019-03-25 15:42:33 +0100

    Align opportunity card layout

--- frontend/src/components/cards/OpportunityCard.vue
+++ frontend/src/components/cards/OpportunityCard.vue
@@ -2,22 +2,22 @@
     <v-fade-transition>
         <v-hover>
             <v-card slot-scope="{ hover }"
-                    :class="`mt-2 clickable elevation-${hover ? 8 : 2} font-weight-light`">
-                <v-card-title class="no-padding-bottom font-weight-light title">
+                    :class="`mt-2 clickable elevation-${hover ? 6 : 2} font-weight-light`">
+                <v-card-title :class="`subheading no-padding-bottom ${getStateColor()}--text`">
                     {{ opportunity.title }}
                 </v-card-title>
-                <v-card-title class="low-padding-left title">
+                <v-card-title class="opp-data">
+                    <v-icon class="mx-1">business</v-icon>
+                    <chip>
+                        {{ getCompanyName() | truncate(30) }}
+                    </chip>
+                    <v-flex xs12/>
                     <v-icon :style="`transform: rotate(${hover ? 180 : 0}deg)`"
                             class="mx-1">
                         timeline
                     </v-icon>
-                    <chip :font-color="getStateColor()">
-                        {{ opportunity.state }}
-                    </chip>
-                    <v-icon class="mx-1">business</v-icon>
-                    <chip slot="activator"
-                          font-color="primary">
-                        {{ getCompanyName() }}
+                    <chip>
+                        {{ lastProgress }}
                     </chip>
                 </v-card-title>
             </v-card>
@@ -47,11 +47,13 @@
                 return this.contactPoints.filter(it => {
                     if (it.opportunity) return it.opportunity.id === this.opportunity.id;
                 });
+            },
+            lastContactPoint() {
+                return this.oppContactPoints[this.oppContactPoints.length - 1];
+            },
+            lastProgress() {
+                return datefns.format(this.lastContactPoint.date, 'DD.MM.YY', {locale: de});
             }
-        },
-        mounted() {
-            console.log(this.oppContactPoints);
-            //this.refreshData();
         },
         methods: {
             ...mapMutations({
@@ -62,9 +64,12 @@
             },
             getStateColor() {
                 switch (this.opportunity.state) {
-                    case 'Lead': return 'primary';
-                    case 'Prospect': return 'warning';
-                    case 'Quote': return 'success';
+                    case 'Lead':
+                        return 'primary';
+                    case 'Prospect':
+                        return 'warning';
+                    case 'Quote':
+                        return 'success';
                 }
             }
         }
@@ -72,11 +77,11 @@
 </script>
 
 <style scoped>
-    .no-padding-bottom {
-        padding-bottom: 0px;
+    .opp-data {
+        padding-left: 10px;
     }
 
-    .low-padding-left {
-        padding-left: 10px;
+    .no-padding-bottom {
+        padding-bottom: 0px;
     }
 </style>
