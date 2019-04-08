18ee62a3aa5454c4081d8d5b9be5ee16f1a355f9
darivs <dtack192@gmail.com>
2019-04-08 13:27:22 +0200

    Display opportunity timeline with it contactpoints

--- frontend/src/views/detail/Opportunity.vue
+++ frontend/src/views/detail/Opportunity.vue
@@ -1,14 +1,52 @@
 <template>
     <v-container>
-        <v-layout row wrap text-xs-center>
-            <v-flex xs1>
-                <v-btn flat small @click="$router.go(-1)">
-                    <v-icon size="22px" class="mr-1" dark>arrow_back</v-icon>
-                    Zurück
-                </v-btn>
-            </v-flex>
-            <v-flex xs10>
-                <span>{{ opportunityId }}</span>
+        <v-fade-transition>
+            <v-layout v-if="!loadingData" row wrap text-xs-left>
+                <v-flex xs12 class="mb-3">
+                    <v-btn flat small @click="$router.go(-1)">
+                        <v-icon size="22px" class="mr-1" dark>arrow_back</v-icon>
+                        Zurück
+                    </v-btn>
+                    <v-divider/>
+                </v-flex>
+                <v-flex xs6>
+                    <span :class="`display-1 ${getStateColor(opportunity.state)}--text`">
+                        {{ opportunity.title }}
+                    </span>
+                    <p class="mt-2 mr-5">{{ opportunity.description }}</p>
+                </v-flex>
+                <v-flex xs6>
+                    <v-timeline>
+                        <v-timeline-item
+                                icon="forum"
+                                :color="`${getStateColor(opportunity.state)}`"
+                                fill-dot
+                                v-for="contactPoint in contactPoints"
+                                v-bind:key="contactPoint.id">
+                            <template v-slot:opposite>
+                                <span>{{ dateFormatted(contactPoint.date) }} <span class="font-italic">mit {{ contactPoint.contact.name }}</span></span>
+                            </template>
+                            <v-card>
+                                <v-card-title :class="`${getStateColor(opportunity.state)}`">
+
+                                    <h2 class="headline white--text font-weight-light">{{ contactPoint.title }}</h2>
+                                </v-card-title>
+                                <v-container>
+                                    <v-layout>
+                                        <v-flex xs12>
+                                            {{ contactPoint.comment }}
+                                        </v-flex>
+                                    </v-layout>
+                                </v-container>
+                            </v-card>
+                        </v-timeline-item>
+                    </v-timeline>
+                </v-flex>
+            </v-layout>
+        </v-fade-transition>
+        <v-layout v-if="loadingData">
+            <v-flex xs12 class="text-xs-center">
+                <v-progress-circular :size="128" color="primary" indeterminate/>
             </v-flex>
         </v-layout>
     </v-container>
@@ -16,6 +54,9 @@
 <script>
     import api from '../../utils/http-common';
     import {mapActions, mapGetters} from 'vuex';
+
+    const datefns = require('date-fns');
+    const de = require('date-fns');
 
     export default {
         data() {
@@ -50,13 +91,28 @@
                 }
             },
             refreshData() {
-                this.getOpportunities().then(() => {
-                    api.get(`point/get/opportunity/${this.opportunityId}`).then(res => {
-                        this.contactPoints = res.data;
-                        this.loadingData = false;
-                    }).catch(err => alert(err));
-                });
+                this.getOpportunities()
+                    .then(() => {
+                        api.get(`point/get/opportunity/${this.opportunityId}`).then(res => {
+                            this.contactPoints = res.data.sort(compareContactPoints);
+                            this.loadingData = false;
+                        }).catch(err => alert(err));
+                    });
+            },
+            dateFormatted(date) {
+                return datefns.format(date, 'DD.MM.YYYY', {locale: de});
             }
+        }
+    }
+
+    function compareContactPoints(a, b) {
+        if (datefns.compareAsc(a.date, b.date) === 0) {
+            if (a.id < b.id)
+                return 1;
+            if (a.id > b.id)
+                return -1;
+        } else {
+            return datefns.compareAsc(b.date, a.date);
         }
     }
 </script>
