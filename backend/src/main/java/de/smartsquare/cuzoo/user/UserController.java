ad544f1f07eb1b24c35078b7e5ce76a1507ae987
darivs <dtack192@gmail.com>
2019-04-16 17:10:31 +0200

    Implement api to get users information by username

--- backend/src/main/java/de/smartsquare/cuzoo/user/UserController.java
+++ backend/src/main/java/de/smartsquare/cuzoo/user/UserController.java
@@ -1,11 +1,14 @@
 package de.smartsquare.cuzoo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,5 +29,16 @@ public class UserController {
                 .stream()
                 .map(User::getUsername)
                 .collect(Collectors.toList()));
+    }
+
+    @RequestMapping("/get/{username}")
+    public final ResponseEntity<?> getUserInformation(@PathVariable String username) {
+        Optional<User> maybeUser = userRepository.findMaybeByUsername(username);
+
+        if (!maybeUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dieser User wurde nicht gefunden!");
+        }
+
+        return ResponseEntity.ok(maybeUser.get());
     }
 }
