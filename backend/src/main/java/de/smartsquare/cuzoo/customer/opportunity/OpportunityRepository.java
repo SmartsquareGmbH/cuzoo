428997e25678c2e7cdce03bdd4a72f639a24335d
darivs <dtack192@gmail.com>
2019-04-24 11:09:59 +0200

    Add sql query to get sorted progress dates of opportunity

--- backend/src/main/java/de/smartsquare/cuzoo/customer/opportunity/OpportunityRepository.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/opportunity/OpportunityRepository.java
@@ -1,9 +1,18 @@
 package de.smartsquare.cuzoo.customer.opportunity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
+
+    @Query("SELECT p.date FROM Opportunity o JOIN o.progress p " +
+            "WHERE o.id = :opportunityId ORDER BY p.date DESC")
+    List<Date> findAllProgressDatesByOpportunityIdOrderByDateDesc(@Param("opportunityId") Long opportunityId);
 
 }
