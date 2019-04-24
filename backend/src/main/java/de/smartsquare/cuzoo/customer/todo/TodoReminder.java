961845f2d85580459fea73a41d81772291303245
darivs <dtack192@gmail.com>
2019-04-24 15:03:52 +0200

    Implement scheduled mail shipping to remind of todos

--- backend/src/main/java/de/smartsquare/cuzoo/customer/todo/TodoReminder.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/todo/TodoReminder.java
@@ -1,20 +1,32 @@
 package de.smartsquare.cuzoo.customer.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.mail.internet.MimeMessage;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Component
 public class TodoReminder {
-    final JavaMailSender sender;
+    private final JavaMailSender sender;
+    private final TaskScheduler taskScheduler;
 
-    public TodoReminder(JavaMailSender sender) {
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoReminder(JavaMailSender sender, TaskScheduler taskScheduler, TodoRepository todoRepository) {
         this.sender = sender;
+        this.taskScheduler = taskScheduler;
+        this.todoRepository = todoRepository;
     }
 
-    void send(String to, String subject, String text) throws Exception {
+    private void send(String to, String subject, String text) throws Exception {
         MimeMessage message = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
@@ -23,6 +35,41 @@ public class TodoReminder {
         helper.setText(text);
 
         sender.send(message);
+    }
+
+    @Scheduled(fixedRate = 1800000) // every 30 minutes
+    void sendTodoReminder() {
+        List<Todo> undoneTodos = todoRepository.findAllByDoneFalse();
+
+        undoneTodos.forEach(todo -> {
+            Date now = new Date(System.currentTimeMillis());
+
+            if (todo.getReminder().after(now)) {
+                taskScheduler.schedule(() -> {
+                    String to = todo.getCreator().getMail();
+                    String companyName = todo.getCompany().getName();
+                    String subject = "[CuZoo] TODO für " + companyName + " fällig";
+
+                    try {
+                        send(to, subject, getMailMessage(todo));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, todo.getReminder());
+            }
+        });
+    }
+
+    private String getMailMessage(Todo todo) {
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.");
+        String faelligkeitsdatum = format.format(todo.getExpiration());
+        String firstname = todo.getCreator().getFullname().split(" ")[0];
+        String companyName = todo.getCompany().getName();
+
+        return "Hey " + firstname + "!\n\n" +
+                "Dein TODO für das Unternehmen " + companyName + " ist am " + faelligkeitsdatum + " fällig!\n\n" +
+                "Beschreibung:\n" + todo.getDescription() + "\n\nMit freundlichen Grüßen\nFriendly Reminder :)\n\n" +
+                "https://cuzoo.smartsquare.de";
     }
 
 }
