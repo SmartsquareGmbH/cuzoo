0701398f2dd014369316bc85d297b7005405087a
darivs <dtack192@gmail.com>
2019-04-24 15:00:55 +0200

    Enable scheduling in application

--- backend/src/main/java/de/smartsquare/cuzoo/Application.java
+++ backend/src/main/java/de/smartsquare/cuzoo/Application.java
@@ -1,10 +1,12 @@
 package de.smartsquare.cuzoo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(final String[] args) {
