d1443e484218618a8a27294b4ca60260dd170dd2
darivs <dtack192@gmail.com>
2019-04-24 15:03:27 +0200

    Add query to find all undone todos

--- backend/src/main/java/de/smartsquare/cuzoo/customer/todo/TodoRepository.java
+++ backend/src/main/java/de/smartsquare/cuzoo/customer/todo/TodoRepository.java
@@ -3,6 +3,11 @@ package de.smartsquare.cuzoo.customer.todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findAllByDoneFalse();
+
 }
