25aa815e0918279d98dc86b653f0c2d2331ceb76
darivs <dtack192@gmail.com>
2019-04-25 10:36:40 +0200

    Add emoji picker component to rate contact points

---
+++ frontend/src/components/core/EmojiPicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <picker set="apple"
+            title="Kontaktpunkt bewerten"
+            emoji="speech_balloon"
+            :i18n="i18n"/>
+</template>
+
+<script>
+    import {Picker} from "emoji-mart-vue"
+
+    export default {
+        components: {
+            Picker
+        },
+        data: () => ({
+            i18n: {
+                search: 'Suche...',
+                notfound: 'Nichts gefunden...',
+                categories: {
+                    search: 'Suchergebnisse',
+                    recent: 'Zuletzt verwendet',
+                    people: 'Leute',
+                    nature: 'Tiere & Natur',
+                    foods: 'Essen & Trinken',
+                    activity: 'Aktivität',
+                    places: 'Reisen & Orte',
+                    objects: 'Objekte',
+                    symbols: 'Symbole',
+                    flags: 'Flaggen',
+                    custom: 'Benutzerdefiniert',
+                }
+            }
+        })
+    }
+</script>
