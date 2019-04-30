c68c0d83637defbecded62458cf332ac7627dde7
darivs <dtack192@gmail.com>
2019-04-30 10:14:15 +0200

    Add test for submitting user information

---
+++ backend/src/test/java/de/smartsquare/cuzoo/user/UserControllerTest.java
@@ -0,0 +1,67 @@
+package de.smartsquare.cuzoo.user;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(username = "drs", password = "secure")
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void initialize() {
+        userRepository.deleteAll();
+        User user = new User("drs", "1234", "", "");
+        userRepository.save(user);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void that_user_information_is_getting_registered() throws Exception {
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.put("/api/user/submit/info/drs")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(getUserInformationInJson());
+
+        this.mockMvc.perform(builder)
+                .andExpect(MockMvcResultMatchers.status()
+                        .isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+        assertThat(userRepository.findAll()
+                .stream()
+                .map(User::getFullname))
+                .containsOnly("drs tck");
+    }
+
+    private String getUserInformationInJson() {
+        return "{\"fullname\": \"drs tck\", \"mail\": \"drs@t.ck\"}";
+    }
+}
