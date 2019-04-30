135b05193e764e0ee25183cb9f38315e3817b949
darivs <dtack192@gmail.com>
2019-04-30 13:26:25 +0200

    Remove autowired annotation

--- backend/src/main/java/de/smartsquare/cuzoo/user/MyUserDetailsService.java
+++ backend/src/main/java/de/smartsquare/cuzoo/user/MyUserDetailsService.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) {
