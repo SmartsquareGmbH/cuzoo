438c2495356f9ea7c87aa4d9ca4fc295af2abbb0
darivs <dtack192@gmail.com>
2019-10-11 15:19:11 +0200

    Add demo login

--- frontend/src/components/core/single/Login.vue
+++ frontend/src/components/core/single/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid fill-height>
-    <v-layout align-center justify-center>
+    <v-layout align-center justify-center row wrap>
       <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
+        <v-card class="elevation-8 mb-4">
           <v-toolbar dark color="primary" class="login-header">
             <v-toolbar-title>Login</v-toolbar-title>
             <v-spacer />
@@ -32,9 +32,11 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer />
-            <v-btn color="primary" @click.native="doLogin">Login</v-btn>
+            <v-btn outline color="primary" @click.native="doDemoLogin">Demo</v-btn>
+            <v-btn color="primary" class="secondary--text" @click.native="doLogin">Login</v-btn>
           </v-card-actions>
         </v-card>
+        <span><strong>Hinweis:</strong> CuZoo ist nicht zum Gebrauch mit dem Smartphone geeignet!</span>
         <v-snackbar v-model="loginFailed" color="error" bottom>
           {{ loginFailedMessage }}
           <v-btn dark flat @click="wrongCredentials = false">
@@ -51,10 +53,9 @@ import { mapMutations } from "vuex"
 import api from "../../../utils/http-common"
 
 export default {
-  name: "login",
   data: () => ({
-    username: null,
-    password: null,
+    username: undefined,
+    password: undefined,
     usernameRules: [(v) => !!v || "Ein Username wird benötigt"],
     passwordRules: [(v) => !!v || "Ein Passwort wird benötigt"],
     loginFailed: false,
@@ -80,6 +81,38 @@ export default {
             authorized: true,
             username: this.username,
             password: this.password,
+          })
+        })
+        .then(() => {
+          this.$router.replace("/dashboard")
+        })
+        .catch((error) => {
+          if (error.message === "Request failed with status code 401") {
+            this.loginFailedMessage = "Die Anmeldedaten sind ungültig!"
+          } else {
+            this.loginFailedMessage = "Es konnte keine Verbindung zum Server hergestellt werden"
+          }
+
+          this.loginFailed = true
+        })
+    },
+    doDemoLogin() {
+      api
+        .post(
+          "security/login",
+          {},
+          {
+            auth: {
+              username: "demo",
+              password: "password",
+            },
+          }
+        )
+        .then(() => {
+          this.storeLogData({
+            authorized: true,
+            username: "demo",
+            password: "password",
           })
         })
         .then(() => {
